dae0911d2c0a21aae411116e986d8f5641beaca5
Dale Warburton <dalewarby23@outlook.com>
2024-01-02 14:38:32 +0000

    cleared the whole of the last javascript and HTML and started again. Added HTML fields to the container to build out the daily tasks

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
-const currentDay = moment().format('dddd, MMMM Do');
-const timeBlocks = [
-    {hour: "9AM, time: 9"},
-    {hour: "10AM, time: 10"},
-    {hour: "11AM, time: 11"},
-    {hour: "12PM, time: 12"},
-    {hour: "1PM, time: 13"},
-    {hour: "2PM, time: 14"},
-    {hour: "3PM, time: 15"},
-    {hour: "4PM, time: 16"},
-    {hour: "5PM, time: 1"},
-];
-
-let tasks = ["", "", "", "", "", "", "", "", "",];
-
-//function to create time blocks
-function createTimeBlocks() {
-
-    $(".container").empty();
-
-    for (let i = 0; i < createTimeBlocks.length; i++) {
-        let dailyTask = tasks[i];
-        let hours = createTimeBlocks[i].hour;
-        let presentHour = parseInt(moment().format("HH"));
-        let currentHour = createTimeBlocks[i].time;
-        let inputElStyle = "";
-
-        if (currentHour < presentHour) {
-
-            inputElStyle = "past";
-        } else {
-            inputElStyle = "future";
-        }
-
-        if (thisHour === presentHour) {
-            inputElStyle = "present";
-        }
-
-        let timeBlockEL = $("<form>").attr("class", "input-group row");
-        let hourContainer = $("<div>").attr("class", "col-2");
-        let hourEl = $("<div>").attr("class", "hour").text(hours).css("text-align", "right");
-        let inputEl = $("<textarea>").attr("class", `form-control textarea ${inputElStyle}`).attr("type", "text").attr("id", "input" + i).val(dailyTask);
-        }
-
-    }
+$(document).ready(function () {
+    //Moment.js code for current date and time
+    let NowMoment = moment().format("MMMM Do YYYY");
+    let displayDate = document.getElementById("currentDay");
+    displayDate.innerHTML = NowMoment;
+    let currentHour = moment().format("HH");
+  
+    // Button function to clear local storage and clear contents
+    $("#clearFieldsBtn").click(function (event) {
+      event.preventDefault;
+      $("textarea").val("");
+      localStorage.clear();
+    });
+  
+    //grabs hour from each time slot and compares it to actual time
+    $(".time-div").each(function () {
+      var timeDiv = $(this).attr("id").split("-")[1];
+      
+      if (currentHour == timeDiv) {
+        $(this).addClass("present");
+        $(this).children(".description").addClass("white-text");
+      } else if (currentHour < timeDiv) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      } else if (currentHour > timeDiv) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      }
+    });
+  
+    //grabs values from time and value divs and saves them to local storage
+    $(".saveBtn").click(function (event) {
+      event.preventDefault();
+      var value = $(this).siblings(".time-block").val();
+      var time = $(this).parent().attr("id").split("-")[1];
+      localStorage.setItem(time, value);
+    });
+  
+    //retrieves items from local storage and sets them in proper places
+    $("#hour-09 .time-block").val(localStorage.getItem("09"));
+    $("#hour-10 .time-block").val(localStorage.getItem("10"));
+    $("#hour-11 .time-block").val(localStorage.getItem("11"));
+    $("#hour-12 .time-block").val(localStorage.getItem("12"));
+    $("#hour-13 .time-block").val(localStorage.getItem("13"));
+    $("#hour-14 .time-block").val(localStorage.getItem("14"));
+    $("#hour-15 .time-block").val(localStorage.getItem("15"));
+    $("#hour-16 .time-block").val(localStorage.getItem("16"));
+    $("#hour-17 .time-block").val(localStorage.getItem("17"));
+  });
